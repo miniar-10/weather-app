33a1c8ad6558b8ba1ae340cc2bce5852316a2a78
miniar-10 <lasmar.miniar9@gmail.com>
2022-07-15 22:06:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState,useEffect} from 'react';
 function App() {
+  const api={
+    key:'383cdd8a99e0b6da45df77070fc5c376',
+    base:'https://api.openweathermap.org/data/2.5/'
+  }
+  const getCurrentweather  =async()=>{
+    const response=await fetch("https://api.openweathermap.org/data/2.5/weather?q=Tunisia&units=metric&APPID=383cdd8a99e0b6da45df77070fc5c376");
+    const data= await response.json();
+    setWeather(data);
+}
+
+useEffect(()=>{getCurrentweather()},[]);
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  
+  const search =async evt => {
+    if (evt.key === "Enter") {
+      const res=await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      // console.log(res)
+      const data= await res.json();
+      console.log(data)
+      setWeather(data);
+      setQuery('');
+        // .then(res => res.json())
+        // .then(result => {
+        //   setWeather(result);
+        //   setQuery('');
+        //   console.log(result);
+        // });
+    }
+  }
+  // weather?lat={lat}&lon={lon}&appid={API key}
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 20) ? 'app warm' : 'app') : 'app'}>
+    <main>
+      <div className="search-box">
+      <input
+       type="text"
+       className='search-bar'
+       placeholder='enter a position'
+       onChange={e => setQuery(e.target.value)}
+       value={query}
+       onKeyPress={search}
+       />
+      </div>
+      {/* {(typeof weather.main == "undefined") && <div>
+          <div className="location-box">
+            <div className="location">El kef, Tunisia</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              15°c
+            
+            </div>
+            <div className="weather">
+           
+              Sunny
+              </div>
+          </div>
+        </div>} */}
+        {(typeof weather.main != "undefined")  &&
+          <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {/* {weather.main.temp} */}
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">
+              {weather.weather[0].main}
+              
+              </div>
+          </div>
+        </div>
+        }
+      
+      
+    </main>
     </div>
   );
 }
